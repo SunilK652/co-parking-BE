54d2b2205296d2845cb1efc86d8084927c5e7a01
Sunil Kulkarni <sunil.kulkarni@valtech.com>
2024-07-04 14:32:19 +0530

    env changes

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 require("dotenv").config();
 
-exports.dbURL = process.env.DB_URL;
+exports.dbURL = 'mongodb+srv://skulkarni652:Sunil123@cluster0.jnmgx4n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
